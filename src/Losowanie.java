39b5c38538960ae8bf14c7a7c4f4330791f482fa
PBrzemo <przemyslawbohdanowicz@gmail.com>
2017-12-28 18:20:27 +0100

    forFream

---
+++ src/Losowanie.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Losowanie {
+
+    public static void main(String[] args) {
+
+        Random r = new Random();
+        int[] x = new int[10];
+        for (int i = 0;  i<10; i++) {
+            x[i] = r.nextInt(100);
+            System.out.println(x[i]);
+        }
+
+    }
+}
