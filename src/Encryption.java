ca09c648e8c3e8f7e153cbd475754441c3f77513
PBrzemo <przemyslawbohdanowicz@gmail.com>
2018-01-03 19:03:21 +0100

    Fream

--- src/Encryption.java
+++ src/Encryption.java
@@ -4,15 +4,15 @@ import javax.crypto.SecretKey;
 import java.util.Scanner;
 
 
-public class Szyfrowanie{
+public class Encryption {
 
 
 
     public static void main(String[] args) throws Exception {
 
-        KeyGenerator keygenerator = KeyGenerator.getInstance("Blowfish");
+        KeyGenerator keygenerator = KeyGenerator.getInstance("AES");// można inne typy algorytmów blowifsh des
         SecretKey secretkey = keygenerator.generateKey();
-        Cipher cipher = Cipher.getInstance("Blowfish");
+        Cipher cipher = Cipher.getInstance("AES");// można inne typy algorytmów blowifsh des
         cipher.init(Cipher.ENCRYPT_MODE, secretkey);
 
         System.out.println("tekst do zaszyfrowania :");
@@ -22,6 +22,7 @@ public class Szyfrowanie{
         System.out.println("-----------------------------------");
         cipher.init(Cipher.DECRYPT_MODE, secretkey);
         byte[] decrypted = cipher.doFinal(encrypted);
+        // to do
         System.out.println("tekst zaszyfrowany : "+ new String(encrypted) + "\n" + "Odszyfrowany: "+ new String(decrypted));
 
     }
